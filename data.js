45bb425466a76610a8254316aea1808b42afac2b
EeroLai <84887419+EeroLai@users.noreply.github.com>
2023-12-05 10:56:49 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,252 @@
+const data = [
+  {
+    type: "小麥",
+    time: 30,
+    quantity: 18,
+  },
+  {
+    type: "草莓",
+    time: 60,
+    quantity: 18,
+  },
+  {
+    type: "番茄",
+    time: 300,
+    quantity: 18,
+  },
+  {
+    type: "棉花",
+    time: 900,
+    quantity: 18,
+  },
+  {
+    type: "馬鈴薯",
+    time: 2700,
+    quantity: 18,
+  },
+  {
+    type: "玉米",
+    time: 5400,
+    quantity: 18,
+  },
+  {
+    type: "甘蔗",
+    time: 3600,
+    quantity: 18,
+  },
+  {
+    type: "葡萄",
+    time: 7200,
+    quantity: 18,
+  },
+  {
+    type: "薰衣草",
+    time: 10800,
+    quantity: 18,
+  },
+  {
+    type: "高麗菜",
+    time: 14400,
+    quantity: 18,
+  },
+  {
+    type: "香草",
+    time: 21600,
+    quantity: 18,
+  },
+  {
+    type: "蘋果樹",
+    time: 600,
+    quantity: 8,
+  },
+  {
+    type: "橘子樹",
+    time: 3600,
+    quantity: 7,
+  },
+  {
+    type: "桃子樹",
+    time: 7200,
+    quantity: 6,
+  },
+  {
+    type: "橡膠樹",
+    time: 10800,
+    quantity: 6,
+  },
+  {
+    type: "咖啡樹",
+    time: 14400,
+    quantity: 6,
+  },
+  {
+    type: "可可樹",
+    time: 28800,
+    quantity: 5,
+  },
+  {
+    type: "櫻桃樹",
+    time: 36000,
+    quantity: 4,
+  },
+  {
+    type: "檸檬樹",
+    time: 28800,
+    quantity: 4,
+  },
+  {
+    type: "牛",
+    time: 300,
+    quantity: 8,
+  },
+  {
+    type: "羊",
+    time: 1500,
+    quantity: 7,
+  },
+  {
+    type: "雞",
+    time: 3600,
+    quantity: 6,
+  },
+  {
+    type: "兔子",
+    time: 14400,
+    quantity: 5,
+  },
+  {
+    type: "蜂蜜",
+    time: 1800,
+    quantity: 6,
+  },
+  {
+    type: "蠶繭",
+    time: 28800,
+    quantity: 5,
+  },
+  {
+    type: "冷衫",
+    time: 90,
+    quantity: 10,
+  },
+  {
+    type: "楓木",
+    time: 4200,
+    quantity: 10,
+  },
+  {
+    type: "梧桐",
+    time: 12600,
+    quantity: 7,
+  },
+  {
+    type: "胡桃",
+    time: 21600,
+    quantity: 7,
+  },
+  {
+    type: "土",
+    time: 3600,
+    quantity: 3,
+  },
+  {
+    type: "礦泉水",
+    time: 3600,
+    quantity: 2,
+  },
+  {
+    type: "鐵",
+    time: 9000,
+    quantity: 5,
+  },
+  {
+    type: "石灰石",
+    time: 14400,
+    quantity: 5,
+  },
+  {
+    type: "銀",
+    time: 21600,
+    quantity: 5,
+  },
+  {
+    type: "金",
+    time: 28800,
+    quantity: 5,
+  },
+  {
+    type: "鋁",
+    time: 25200,
+    quantity: 3,
+  },
+  {
+    type: "白金寶石",
+    time: 27000,
+    quantity: 3,
+  },
+  {
+    type: "秋刀魚",
+    time: 7200,
+    quantity: 6,
+  },
+  {
+    type: "蝦子",
+    time: 10800,
+    quantity: 6,
+  },
+  {
+    type: "龍蝦",
+    time: 10800,
+    quantity: 6,
+  },
+  {
+    type: "鮪魚",
+    time: 14400,
+    quantity: 6,
+  },
+  {
+    type: "鮭魚",
+    time: 14400,
+    quantity: 6,
+  },
+  {
+    type: "鯊魚",
+    time: 25200,
+    quantity: 6,
+  },
+  {
+    type: "鯨魚",
+    time: 25200,
+    quantity: 6,
+  },
+  {
+    type: "羊皮",
+    time: 1200,
+    quantity: 5,
+  },
+  {
+    type: "牛肉",
+    time: 1800,
+    quantity: 5,
+  },
+  {
+    type: "蘆筍",
+    time: 2700,
+    quantity: 5,
+  },
+  {
+    type: "牛皮",
+    time: 3600,
+    quantity: 5,
+  },
+  {
+    type: "馬皮",
+    time: 14400,
+    quantity: 5,
+  },
+  {
+    type: "雞肉",
+    time: 10800,
+    quantity: 5,
+  },
+];
